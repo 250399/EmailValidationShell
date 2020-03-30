cbf0d16bd5b818c3003989762835cb6667f89881
Kunal <kunaldeshmukh2503@gmail.com>
2020-03-30 08:49:49 -0700

    use case 5

--- regex.sh
+++ regex.sh
@@ -3,4 +3,4 @@
 
 echo welcome to email validation program
 read -p"Enter characters" char
-[[ $char =~ ^[a-zA-Z0-9]+[.\-+_a-zA-Z0-9]*@[a-zA-Z]+[.]?[a-zA-Z]+[\.]?([a-z]{2})?$ ]] && echo Valid || echo Invalid
+[[ $char =~ ^[a-zA-Z0-9]+[.\-+_a-zA-Z0-9]*@[a-zA-Z]+[.]?[a-zA-Z]{2,4}[\.]?([a-z]{2})?$ ]] && echo Valid || echo Invalid
